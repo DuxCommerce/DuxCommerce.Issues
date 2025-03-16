27f85f4e665592b0998992535659ac4fcfe5323a
duxonline <duxcommerce@gmail.com>
2025-03-16 22:19:35 +1100

    Display customer fields

--- src/DuxCommerce.Storefront/Views/CustomerField/VmBuilders/CustomerFieldsBuilder.cs
+++ src/DuxCommerce.Storefront/Views/CustomerField/VmBuilders/CustomerFieldsBuilder.cs
@@ -2,27 +2,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DuxCommerce.OrchardCore.Catalog.Products;
+using DuxCommerce.StoreBuilder.Catalog.Core;
 using DuxCommerce.StoreBuilder.Catalog.DataStores;
 using DuxCommerce.StoreBuilder.Catalog.DataTypes;
 using DuxCommerce.StoreBuilder.Catalog.DomainTypes;
 using DuxCommerce.StoreBuilder.Catalog.Requests;
+using DuxCommerce.StoreBuilder.Catalog.UseCases;
 using DuxCommerce.Storefront.Views.AdminProduct.ViewModels;
 using DuxCommerce.Storefront.Views.CustomerField.ViewModels;
+using DuxCommerce.Storefront.Views.ProductOption.ViewModels;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using OrchardCore.ContentManagement;
 
 namespace DuxCommerce.Storefront.Views.CustomerField.VmBuilders;
 
-public class CustomerFieldsBuilder(IProductStore productStore, ICustomerFieldsStore customerFieldsStore)
+public class CustomerFieldsBuilder(
+    IProductStore productStore,
+    ICustomerFieldsStore customerFieldsStore,
+    CustomerFieldsUseCases customerFieldsUseCases)
 {
     public async Task<CustomerFieldsVm> BuildIndexModel(string productId)
     {
+        var fields = await GetAllFields(productId);
+
         var productItem = await productStore.GetItem<ContentItem>(productId);
 
         return new CustomerFieldsVm
         {
             Product = productItem.As<ProductPart>().Row,
-            Fields = [],
+            Fields = fields,
             Links = new ProductLinksVm { ContentItem = productItem, FieldsLink = true }
         };
     }
@@ -48,7 +56,7 @@ public class CustomerFieldsBuilder(IProductStore productStore, ICustomerFieldsSt
     {
         var fieldsRow = await customerFieldsStore.GetByProductId(productId);
         var fieldRow = fieldsRow.PrivateFields.Single(x => x.Id == fieldId);
-        
+
         var choices = (fieldRow.Option?.Choices ?? [])
             .OrderBy(x => x.DisplayOrder)
             .ThenBy(x => x.CreatedAtUtc);
@@ -60,6 +68,21 @@ public class CustomerFieldsBuilder(IProductStore productStore, ICustomerFieldsSt
             Choices = choices,
             FieldTypes = GetFieldTypes()
         };
+    }
+
+    private async Task<IEnumerable<CustomerFieldVm>> GetAllFields(string productId)
+    {
+        var customerFields = await customerFieldsStore.GetByProductId(productId);
+        // var sharedFields = await customerFieldsUseCases.GetSharedFields(customerFields);
+        var sharedFields = new List<CustomFieldRow>();
+        
+        var sharedFieldsVms = CustomerFieldsCore.getSharedFields(customerFields, sharedFields)
+            .Select(x => new CustomerFieldVm { Field = x, Shared = true });
+
+        var privateFieldVms = customerFields.PrivateFields
+            .Select(x => new CustomerFieldVm { Field = x });
+
+        return sharedFieldsVms.Concat(privateFieldVms).OrderBy(x => x.Field.DisplayOrder);        
     }
 
     public async Task<PrivateFieldVm> BuildEditModel(string productId, string fieldId)
@@ -78,8 +101,8 @@ public class CustomerFieldsBuilder(IProductStore productStore, ICustomerFieldsSt
             IsRequired = fieldRow.IsRequired,
             DisplayOrder = fieldRow.DisplayOrder
         };
-    }    
-    
+    }
+
     private IEnumerable<SelectListItem> GetFieldTypes()
     {
         return new List<SelectListItem>
